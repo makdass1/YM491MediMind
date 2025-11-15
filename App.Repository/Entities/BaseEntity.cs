d2404166bad4796934e3b52146a54a1d2bd3a0e7
Mustafa Akdaş <makdas163@gmail.com>
2025-11-16 01:29:03 +0300

    EntityCreated

---
+++ App.Repository/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repository.Entities
+{
+    public class BaseEntity<T> where T : struct
+    {
+        public T Id { get; set; }
+    }
+}
