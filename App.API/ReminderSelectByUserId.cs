e279dbd95a6bf454236638cbb8c7c9b3d826ffab
Mustafa Akdaş <makdas163@gmail.com>
2026-01-07 18:48:02 +0300

    Idye göre kullanıcı reminderları listelendi

---
+++ App.API/ReminderSelectByUserId.cs
@@ -0,0 +1,30 @@
+﻿using App.Service.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.API.Controllers
+{
+    [Route("api/reminderselectbyId")]
+    [ApiController]
+    public class ReminderSelectByUserId(ReminderService reminderService) : ControllerBase
+    {
+        private readonly ReminderService _reminderService = reminderService;
+
+        [HttpGet("{userId}")]
+    public async Task<IActionResult> GetRemindersBySpecificUserId(int userId)
+    {
+        try
+        {
+
+            var reminders = await _reminderService.GetRemindersByUserIdAsync(userId);
+
+
+            return Ok(reminders);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
+}
+}
